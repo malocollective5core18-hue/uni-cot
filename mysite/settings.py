790e08d43b9cdcb31e7d02fe6dc62508014de86e
UNI-COT <malo.collective5.core18@gmail.com>
2026-04-15 23:06:24 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,1 +1,9 @@
 from .mysite.settings import *  # noqa: F401,F403
+
+MIDDLEWARE = [
+  'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',  # Should be right below SecurityMiddleware
+  # ...
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
