cd426a0bbda1f4bcc65f71f1d970d9d251bbacb7
UNI-COT <malo.collective5.core18@gmail.com>
2026-04-16 13:47:54 +0300

    Delete mysite directory

--- mysite/wsgi.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import sys
-
-project_root = Path(__file__).resolve().parent.parent
-django_root = project_root / 'mysite'
-if str(django_root) not in sys.path:
-    sys.path.insert(0, str(django_root))
-
-from .mysite.wsgi import application
